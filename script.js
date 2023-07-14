258db2433d05d544dbccd8001525c3b230527ab4
wagnerjr567 <wagnerjr567@rede.ulbra.br>
2023-07-14 16:42:20 -0300

    feat(home): animacão botão curtir

---
+++ script.js
@@ -0,0 +1,23 @@
+var likeIcons = document.querySelectorAll('.bi.bi-heart');
+
+likeIcons.forEach(function(likeIcon) {
+    var postId = likeIcon.postId;
+    var liked = localStorage.getItem(postId);
+
+    if(liked === 'true') {
+        likeIcon.classList.add('bi-heart-fill');
+        likeIcon.classList.remove('bi-heart');
+    }
+
+    likeIcon.addEventListener('click', function() {
+        this.classList.toggle('bi-heart');
+        this.classList.toggle('bi-heart-fill');
+
+        if (this.classList.contains('bi-heart-fill')){
+            localStorage.setItem(postId, 'true');
+        } else {
+            localStorage.setItem( postId, 'false');
+        }
+
+    });
+});
